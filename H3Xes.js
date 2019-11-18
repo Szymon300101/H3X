f540ae1e88a2471ede55f1ee5246f77a5e294adf
Szymon <45321078+Szymon300101@users.noreply.github.com>
2019-11-18 19:26:30 +0100

    Update H3Xes.js

--- H3Xes.js
+++ H3Xes.js
@@ -7,26 +7,30 @@ const scale_power=2;       //potęga do której ponoszony jest szum przy skalowa
 const opacity=60;            //średnia przezroczystość sześcokątów
 
 function setup() {
-  createCanvas(700, 500);
-  noStroke();
+  createCanvas(windowWidth, windowHeight);
+  //noStroke();
   colorMode(HSB,255);
- 
+  
+ generate();
 }
 
 function draw() {
-  background(70);
-  hex_shape(200,100,0.5,200,255);
+
 }
 
 function generate()
 {
+  noStroke();
+  let cnt=0;
   background(255);
-  for(let x=0;x<width;x+=grid)
+  for(let x=0.0;x<width;x+=grid)
   {
-    for(let y=0;y<height;y+=grid)
+    for(let y=0.0;y<height;y+=grid)
     {
       if(random(low,high)<pow(noise(x*nScale,y*nScale),power))  //decydowanie czy pojawi się sześcokąt
       {
+        //point(x*grid,y*grid);
+        cnt++;
         //jeśli losowa wartość z przedziału jest mniejsza od potęgi szumu perlina dla danego 'x' i 'y'
         //w danym punkcie zaraz pojawi się sześcoikąt (czyli im większa wartośc szumu, tym większa szansa że pojawi się sześciokąt
   
@@ -34,20 +38,26 @@ function generate()
       }
     }
   }
+  console.log(cnt);
 }
 
 function hex_shape(x,y,size,hue,alpha)
 {
-  translate(x,y);
-  scale(size);
+  //scale(size);
   fill(hue,255,255,alpha);
   beginShape();
-  vertex(0,-100);
-  vertex(86,-50);
-  vertex(86,50);
-  vertex(0,100);
-  vertex(-86,50);
-  vertex(-86,-50);
-  vertex(0,-100);
+  vertex(x,y-100*size);
+  vertex(x+86*size,y-50*size);
+  vertex(x+86*size,y+50*size);
+  vertex(x,y+100*size);
+  vertex(x-86*size,y+50*size);
+  vertex(x-86*size,y-50*size);
+  vertex(x,y-100*size);
   endShape();
+}
+
+function keyPressed()
+{
+  //println(keyCode);
+  if(keyCode==32) {generate();}
 }
